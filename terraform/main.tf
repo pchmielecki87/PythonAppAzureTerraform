105747de9442e7ae67c63141a11f14a81b982ebe
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-12-02 01:06:51 +0100

    update tf ASP

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,7 @@ resource "azurerm_service_plan" "asp" {
   name                = "${var.prefix}-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
   os_type = "Linux"
-  kind    = "Linux"
-
   sku_name = "F1"   # Free tier App Service Plan
 }
 
