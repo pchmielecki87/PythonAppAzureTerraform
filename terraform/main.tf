459973c436b6b5ee39e3a031f7df44306c3e17df
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2025-12-04 00:29:34 +0100

    restructure TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,7 +55,7 @@ resource "azurerm_application_insights" "ai" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
-  workspace_id        = azurerm_log_analytics_workspace.law.id
+  # workspace_id        = azurerm_log_analytics_workspace.law.id
   # retention_in_days optional
 
   # depends_on = [azurerm_log_analytics_workspace.law]
